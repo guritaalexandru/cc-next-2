db21a655619d6a7d101f84b5e88be5a6304aac6d
guritaalexandru <a_gurita@yahoo.com>
2023-04-27 18:47:32 +0300

    added openai

--- pages/api/answer.js
+++ pages/api/answer.js
@@ -1,4 +1,5 @@
-import {sendBadRequest, sendMethodNotAllowed} from "@/js/utils/apiMethods";
+import {sendBadRequest, sendMethodNotAllowed, sendOk} from "@/js/utils/apiMethods";
+import {openai} from "@/lib/openai";
 
 const SYSTEM_PROMPTS = {
 	SIMPLE_ASSISTANT: {
@@ -21,6 +22,21 @@ const SYSTEM_PROMPTS = {
 	},
 };
 
+const ERRORS = {
+	DATABASE_ERROR: {
+		type: 'database_error',
+		message: 'Error while processing the request.',
+	},
+	WRONG_CONVERSATION_TYPE: {
+		type: 'wrong_conversation_type',
+		message: 'The conversation type is not known.',
+	},
+	OPEN_AI_ERROR: {
+		type: 'open_ai_error',
+		message: 'Error while processing the request.',
+	},
+};
+
 const chatCompletion = async (messagesArray, max_tokens, temperature) => {
 	const rawResponse = await openai.createChatCompletion({
 		model: 'gpt-3.5-turbo',
@@ -32,12 +48,38 @@ const chatCompletion = async (messagesArray, max_tokens, temperature) => {
 	return rawResponse?.data?.choices[0];
 }
 
+const converseChat = async (res, inputChat, useCase) => {
+	try{
+		const MAX_MEMORY = 3;
+		let userMessagesArray = [];
+
+		if (inputChat.length > MAX_MEMORY) {
+			userMessagesArray = inputChat.slice(-MAX_MEMORY);
+		}
+		else {
+			userMessagesArray = inputChat;
+		}
+
+		const messagesArray = [
+			useCase.MESSAGE,
+			...userMessagesArray
+		];
+
+		const response = await chatCompletion(messagesArray, useCase.MAX_TOKENS, useCase.TEMPERATURE);
+		return sendOk(res, response);
+	}
+	catch(error) {
+		console.error(error);
+		return sendBadRequest(res, ERRORS.OPEN_AI_ERROR.type, ERRORS.OPEN_AI_ERROR.message);
+	}
+}
+
 const converse = (res, messages, type) => {
 	switch (type) {
 		case SYSTEM_PROMPTS.SIMPLE_ASSISTANT.TYPE:
-			return;
+			return converseChat(res, messages, SYSTEM_PROMPTS.SIMPLE_ASSISTANT);
 		case SYSTEM_PROMPTS.MICHAEL_SCOTT.TYPE:
-			return;
+			return converseChat(res, messages, SYSTEM_PROMPTS.MICHAEL_SCOTT);
 		default:
 			return sendBadRequest(res, 'wrong_conversation_type');
 	}
